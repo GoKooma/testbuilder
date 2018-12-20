03cea0b452c6fb50d88a1264c653776b1fb72ca8
KweonMo Koo <mikekoo2010@gmail.com>
2018-12-19 17:16:36 -0800

    Step 3 in progres
    Masetro section is showing unexpected behavior. But other sections passed the test.

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,69 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-};
 
-
+  // Check the prefix to identify the potential matching network
+  // then check the length to identify the matching network
+  if (cardNumber[0] === '3') {
+  	// if first prefix is 3, then check which network (Diner's Club or American Express) it belongs
+  	if (cardNumber.length === 14) {
+  		if ((cardNumber[1] === '8') || (cardNumber[1] === '9')) {
+  			return 'Diner\'s Club';
+  		}
+  	} else if (cardNumber.length === 15) {
+  		if ((cardNumber[1] === '4') || (cardNumber[1] === '7')) {
+  			return 'American Express';
+  		}
+  	}
+  } else if (cardNumber[0] === '4') {
+  	if ((cardNumber.length === 13) || (cardNumber.length === 16) || (cardNumber.length === 19)) {
+  		return 'Visa';
+  	}
+  } else if (cardNumber[0] === '5') {
+  	if (cardNumber.length === 16) {
+  		// use for loop to check if the prefix matches the range of matching network
+  		for (var i = 1; i < 6; i++) {
+  			if (i === Number(cardNumber[1])) {
+  				return 'MasterCard';
+  			}
+  		}
+  	}
+  } else if (cardNumber.substring(0,4) === '6011') {
+  	if ((cardNumber.length === 16) || (cardNumber.length === 19)) {
+  		return 'Discover';
+  	}
+  } else if (cardNumber.substring(0,7) === '644-649') {
+  	if ((cardNumber.length === 16) || (cardNumber.length === 19)) {
+  		return 'Discover';
+  	}
+  } else if (cardNumber.substring(0,2) === '65') {
+  	if ((cardNumber.length === 16) || (cardNumber.length === 19)) {
+  		return 'Discover';
+  	}
+  } else if (cardNumber.substring(0,4) === '5018') {
+  	for (var i = 12; i < 20; i++) {
+  			if (i === cardNumber.length) {
+  				return 'Maestro';
+  			}
+  		}
+  } else if (cardNumber.substring(0,4) === '5020') {
+  	for (var i = 12; i < 20; i++) {
+  			if (i === cardNumber.length) {
+  				return 'Maestro';
+  			}
+  		}
+  } else if (cardNumber.substring(0,4) === '5038') {
+  	for (var i = 12; i < 20; i++) {
+  			if (i === cardNumber.length) {
+  				return 'Maestro';
+  			}
+  		}
+  } else if (cardNumber.substring(0,4) === '6304') {
+  	for (var i = 12; i < 20; i++) {
+  			if (i === cardNumber.length) {
+  				return 'Maestro';
+  			}
+  		}
+  }
+  
+}
